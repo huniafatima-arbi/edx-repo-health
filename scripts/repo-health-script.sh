7bf1896509e51a22858adebba5ed03d10ce37e71
Hunia Fatima <hunia.fatima@arbisoft.com>
2024-09-13 11:53:22 +0500

    fix: upgrading the setuptools

--- scripts/repo-health-script.sh
+++ scripts/repo-health-script.sh
@@ -15,7 +15,7 @@ fi
 # Get list of repos in given organizations
 ##########################################
 
-pip install setuptools
+pip install --upgrade setuptools
 pip-sync -q repo_tools/requirements/base.txt
 pip install -q -e repo_tools
 cd "$WORKSPACE"
